9c33538c99b0c29e8c55d8a3fbd8a8907d9c34b8
knightws6 <jdotsonjr@aol.com>
2026-03-27 21:15:57 -0500

    Upgrade AWS provider version to 6.0

--- main.tf
+++ main.tf
@@ -2,13 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-west-2" # or your preferred region
+  # Configuration options
+  region = "us-west-2"
 }
 
 #random suffix, s3 bucket names must be globally unique
