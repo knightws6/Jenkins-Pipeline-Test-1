56abae49eae2f78bb55c7b675cea9ba0d25c5eee
knightws6 <jdotsonjr@aol.com>
2026-03-28 10:56:41 -0500

    Rewrite

--- main.tf
+++ main.tf
@@ -4,20 +4,22 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
 }
 
 provider "aws" {
-  # Configuration options
   region = "us-east-2"
 }
 
-#random suffix, s3 bucket names must be globally unique
 resource "random_id" "bucket_id" {
   byte_length = 4
 }
 
 resource "aws_s3_bucket" "test" {
-  bucket = lower("Jenkins-test-bucket-bucket-${random_id.bucket_id.hex}")   # must be globally unique, hence random_id
+  bucket        = lower("jenkins-test-bucket-${random_id.bucket_id.hex}")
   force_destroy = true
 }
