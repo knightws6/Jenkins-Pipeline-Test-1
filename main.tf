8e292da41261726a3e13037884da80c814ca53f9
knightws6 <jdotsonjr@aol.com>
2026-03-29 11:37:03 -0500

    main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-#S3 bucket ID random suffix
+# S3 bucket ID random suffix
 resource "random_id" "bucket_id" {
   byte_length = 4
 }
 
 # S3 bucket name
-resource "aws_s3_bucket" "jenkins-test1" {
-  bucket = lower("jenkins-test1-${random_id.bucket_id.hex}")
+resource "aws_s3_bucket" "jenkins_test1" {
+  bucket        = lower("jenkins-test1-${random_id.bucket_id.hex}")
   force_destroy = true
 }
