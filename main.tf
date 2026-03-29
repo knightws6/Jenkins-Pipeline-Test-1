36f3f9aef7fd1bff1b51c799d7361affd8280762
knightws6 <jdotsonjr@aol.com>
2026-03-29 12:09:08 -0500

    Main.tf
    Added a public read policy for the S3 bucket.

--- main.tf
+++ main.tf
@@ -3,8 +3,26 @@ resource "random_id" "bucket_id" {
   byte_length = 4
 }
 
-# S3 bucket name
+# S3 bucket ID
 resource "aws_s3_bucket" "jenkins_test1" {
   bucket        = lower("jenkins-test1-${random_id.bucket_id.hex}")
   force_destroy = true
+}
+
+# Bucket public policy
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.jenkins_test1.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = ["s3:GetObject"]
+        Resource  = "${aws_s3_bucket.jenkins_test1.arn}/*"
+      }
+    ]
+  })
 }
